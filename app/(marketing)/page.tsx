96865cab0eb1f8a409e9d1e71ac369bb196e7a90
Oleg Pimenov <pimenov.o@gmail.com>
2022-11-14 17:27:12 +0300

    chore: added node engines

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+export default async function IndexPage() {
+    return (
+        <>
+            <section>
+                <h1>Platform for Everyone</h1>
+            </section>
+        </>
+    );
+}
---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+    return (
+        <>
+            <title>VXControl</title>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width" />
+            <meta name="description" content="An open source application." />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="" />
+            <meta property="og:image" content="" />
+        </>
+    );
+}
