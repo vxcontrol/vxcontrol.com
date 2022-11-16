04e4a16d8987eeb62bb16c48780717d3fc1e48e4
Alexey Solyanik <solyanik.alexey@gmail.com>
2022-11-16 09:53:48 +0300

    fix: TelegramBlock - z-index;

--- components/screens/home/components/Blocks/TelegramBlock.tsx
+++ components/screens/home/components/Blocks/TelegramBlock.tsx
@@ -14,16 +14,16 @@ export const TelegramBlock: FC<TelegramBlockProps> = (props) => {
     return (
         <section className={classNames('container', className)}>
             <div className={'bg-panel p-[60px] medium:p-[40px] small:p-[40px] mobile:p-[32px] flex flex-col relative rounded-[20px] overflow-hidden'}>
-                <h1 className={'text-h-medium-x mb-4'}>
+                <h1 className={'text-h-medium-x mb-4 z-[2]'}>
                     SOLDR в Телеграме
                 </h1>
 
-                <div className={'text-t-medium text-main-light-gray mb-[40px]'}>
+                <div className={'text-t-medium text-main-light-gray mb-[40px] z-[2]'}>
                     Узнать больше о продукте и задать вопрос экспертам можно в чате
                 </div>
 
                 <a
-                    className={'text-t-medium px-[50px] py-[20px] bg-blue-button rounded-[54px] w-fit hover:bg-blue'}
+                    className={'text-t-medium px-[50px] py-[20px] bg-blue-button rounded-[54px] w-fit hover:bg-blue z-[2]'}
                     href={TELEGRAM_LINK}
                     target={'_blank'}
                     rel={'noopener noreferrer'}
@@ -31,7 +31,7 @@ export const TelegramBlock: FC<TelegramBlockProps> = (props) => {
                     Перейти
                 </a>
 
-                <AtSymbolIcon className={'absolute right-0 top-0 mobile:right-[-50%] mobile:scale-50'} />
+                <AtSymbolIcon className={'absolute right-0 top-0 mobile:right-[-50%] mobile:scale-50 z-[1]'} />
             </div>
         </section>
     )
