ebfb3b1c838098e1e18f80cfcabf170d738d0cfc
Oleg Pimenov <pimenov.o@gmail.com>
2022-11-17 16:24:36 +0300

    chore: upd eslint settings

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # vxcontrol.com
+
+
+# Getting started
+Run the following command on your local environment:
+
+```bash
+npm install
+```
+
+Then, you can run locally in development mode with live reload:
+```bash
+npm preview
+```
+
+# Deploy to production
+
+```git push origin <x.x.x>```
--- src/components/CookieNotice.tsx
+++ src/components/CookieNotice.tsx
@@ -7,7 +7,7 @@ import CloseIcon from './assets/close.svg';
 import useTranslation from 'next-translate/useTranslation';
 import Trans from 'next-translate/Trans';
 
-export type CookieNoticeProps = {}
+export type CookieNoticeProps = {};
 
 export const CookieNotice: FC<CookieNoticeProps> = (props) => {
     const {} = props;
@@ -17,15 +17,19 @@ export const CookieNotice: FC<CookieNoticeProps> = (props) => {
     const [allowed, setAllowed] = useState<boolean>(true);
 
     useEffect(() => {
-        setAllowed(Cookies.get(config.cookieNotice.cookiesAllowedName) === 'true');
+        setAllowed(
+            Cookies.get(config.cookieNotice.cookiesAllowedName) === 'true'
+        );
     }, []);
 
     const allow = () => {
         const domain = new URL(window.location.href).hostname;
 
         Cookies.set(config.cookieNotice.cookiesAllowedName, 'true', {
-            expires: new Date(new Date().valueOf() + config.cookieNotice.expires),
-            domain,
+            expires: new Date(
+                new Date().valueOf() + config.cookieNotice.expires
+            ),
+            domain
         });
 
         setAllowed(true);
@@ -36,26 +40,47 @@ export const CookieNotice: FC<CookieNoticeProps> = (props) => {
     }
 
     return (
-        <div className={classNames(
-            'bg-main-dark rounded-[20px] p-[32px] fixed bottom-0 right-0 mb-[24px] mr-[24px] w-[488px] flex flex-col z-[9]',
-            'small:w-[calc(100%_-_40px)] small:mr-[20px] small:mb-[20px] small:left-[20px]',
-            'mobile:w-[calc(100%_-_20px)] mobile:p-[20px] mobile:mr-[10px] mobile:mb-[10px] mobile:left-[10px]'
-        )}>
-            <h1 className={'text-h-small-x medium:text-t-medium small:text-t-medium mobile:text-t-medium mb-[12px]'}>
+        <div
+            className={classNames(
+                'fixed bottom-0 right-0 z-[9] mb-[24px] mr-[24px] flex w-[488px] flex-col rounded-[20px] bg-main-dark p-[32px]',
+                'small:left-[20px] small:mr-[20px] small:mb-[20px] small:w-[calc(100%_-_40px)]',
+                'mobile:left-[10px] mobile:mr-[10px] mobile:mb-[10px] mobile:w-[calc(100%_-_20px)] mobile:p-[20px]'
+            )}
+        >
+            <h1
+                className={
+                    'mb-[12px] text-h-small-x mobile:text-t-medium small:text-t-medium medium:text-t-medium'
+                }
+            >
                 {t('cookie_notice_popup_title')}
             </h1>
 
-            <div className={'text-t-small medium:text-t-small-x small:text-t-small-x mobile:text-t-small-x mb-[32px]'}>
+            <div
+                className={
+                    'mb-[32px] text-t-small mobile:text-t-small-x small:text-t-small-x medium:text-t-small-x'
+                }
+            >
                 <Trans
                     i18nKey="common:cookie_notice_popup_message"
                     components={{
-                        component: <a href={'/cookie-notice'} target={'_blank'} rel="noreferrer" className={'text-main-light-gray hover:text-hover'} />,
+                        component: (
+                            <a
+                                href={'/cookie-notice'}
+                                target={'_blank'}
+                                rel="noreferrer"
+                                className={
+                                    'text-main-light-gray hover:text-hover'
+                                }
+                            />
+                        )
                     }}
                 />
             </div>
 
             <button
-                className={'text-t-medium px-[50px] py-[20px] bg-blue-button rounded-[54px] w-fit hover:bg-blue z-[2]'}
+                className={
+                    'z-[2] w-fit rounded-[54px] bg-blue-button px-[50px] py-[20px] text-t-medium hover:bg-blue'
+                }
                 onClick={allow}
             >
                 {t('cookie_notice_popup_allow_all')}
@@ -63,14 +88,14 @@ export const CookieNotice: FC<CookieNoticeProps> = (props) => {
 
             <button
                 className={classNames(
-                    'font-bold uppercase border border-main-light-gray w-[30px] h-[30px] rounded-[30px] text-main-light-gray',
-                    'flex items-center justify-center absolute right-[10px] top-[10px]',
-                    'hover:text-hover hover:border-hover',
+                    'h-[30px] w-[30px] rounded-[30px] border border-main-light-gray font-bold uppercase text-main-light-gray',
+                    'absolute right-[10px] top-[10px] flex items-center justify-center',
+                    'hover:border-hover hover:text-hover'
                 )}
                 onClick={() => setAllowed(true)}
             >
                 <CloseIcon />
             </button>
         </div>
-    )
-}
+    );
+};
