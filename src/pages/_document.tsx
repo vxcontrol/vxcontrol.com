11e1f12a68e594131a34edfbb2926aa30e8dcfbe
Alexey Solyanik <solyanik.alexey@gmail.com>
2022-11-18 14:53:03 +0300

    fit: ya metric was added;

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,8 +18,19 @@ export default class AppDocument extends Document {
                     <meta name="viewport" content="width=device-width" />
                 </Head>
                 <body>
-                <Main />
-                <NextScript />
+                    <Main />
+                    <NextScript />
+
+                    <script type="text/javascript" dangerouslySetInnerHTML={{
+                        __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(91183581, "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});`
+                    }}>
+                    </script>
+
+                    <noscript>
+                        <div>
+                            <img src="https://mc.yandex.ru/watch/91183581" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+                        </div>
+                    </noscript>
                 </body>
             </Html>
         );
